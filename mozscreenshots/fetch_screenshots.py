8247ee161341d79303c7faa839994c3191f5e546
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2015-05-27 23:05:31 -0700

    Don't hardcode 'try' on fetch_screenshots.py

--- mozscreenshots/fetch_screenshots.py
+++ mozscreenshots/fetch_screenshots.py
@@ -4,8 +4,8 @@ import os
 import os.path
 import requests
 
-def fetch_screenshots(rev):
-    resultset_url = 'https://treeherder.mozilla.org/api/project/try/resultset/?count=1&full=true&revision=' + rev
+def fetch_screenshots(rev, project):
+    resultset_url = 'https://treeherder.mozilla.org/api/project/%s/resultset/?count=1&full=true&revision=%s' % (project, rev)
     resultset = requests.get(resultset_url).json()
 
     if resultset['meta']['count'] == 0:
@@ -15,19 +15,19 @@ def fetch_screenshots(rev):
     result_set_id = resultset['results'][0]['id']
     print "Result Set ID: %d" % result_set_id
 
-    jobs_url = 'https://treeherder.mozilla.org/api/project/try/jobs/?count=2000&result_set_id=%d' % result_set_id
+    jobs_url = 'https://treeherder.mozilla.org/api/project/%s/jobs/?count=2000&result_set_id=%d' % (project, result_set_id)
     jobs = requests.get(jobs_url).json()
 
     for job in jobs['results']:
         if job['job_type_name'] != 'Mochitest Browser Chrome':
             continue
         job_id = job['id']
-        fetch_artifacts_for_job(job_id, job['platform'], rev)
+        fetch_artifacts_for_job(job_id, job['platform'], rev, project)
 
 
-def fetch_artifacts_for_job(job_id, platform, rev):
+def fetch_artifacts_for_job(job_id, platform, rev, project):
     print "Fetching artifacts for job id: %d" % job_id
-    artifacts_url = 'https://treeherder.mozilla.org/api/project/try/artifact/?job_id=%d&name=Job+Info&type=json' % job_id
+    artifacts_url = 'https://treeherder.mozilla.org/api/project/%s/artifact/?job_id=%d&name=Job+Info&type=json' % (project, job_id)
     artifacts = requests.get(artifacts_url).json()
 
     for artifact in artifacts:
@@ -64,10 +64,15 @@ def download_screenshot(url, filepath):
     file.write(image.content)
     file.close()
 
+def cli():
+    parser = argparse.ArgumentParser(description='Fetch screenshots from automation')
+    parser.add_argument('-r', '--rev', required=True,
+                        help="Revision to fetch screenshots from")
+    parser.add_argument('--project', default="try",
+                        help="Project that the revision is from. [Default=try]")
+    args = parser.parse_args()
+    fetch_screenshots(args.rev, args.project)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Fetch screenshots from automation')
-    parser.add_argument('-r', '--rev')
-    args = parser.parse_args()
-
-    fetch_screenshots(args.rev)
+    cli()
