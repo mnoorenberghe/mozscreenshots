fe181bc693edb4bb152e709a69b22277514d3e21
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2016-08-05 16:31:06 -0700

    list_nightlies: dedupe Nightlies as they can be returned by multiple days

--- mozscreenshots/list_nightlies.py
+++ mozscreenshots/list_nightlies.py
@@ -13,19 +13,22 @@ project = "mozilla-central"
 base = datetime.date.today()
 numdays = 10
 resultsets = []
+nightly_revs = set()
+
 for offset in range(0, numdays):
     d = base - datetime.timedelta(days=offset)
     print d.isoformat()
-    nightly_revs = nightly_revs_for_date(project, d.isoformat())
-    print nightly_revs
+    revs = nightly_revs_for_date(project, d.isoformat())
+    print revs
+    nightly_revs.update(revs)
 
-    for rev in nightly_revs:
-        resultset_response = resultset_response_for_push(project, rev)
-        if not resultset_response:
-            continue
-        resultset = resultset_response['results'][0]
-        print resultset['push_timestamp'], resultset['revision']
-        resultsets.append(resultset)
+for rev in nightly_revs:
+    resultset_response = resultset_response_for_push(project, rev)
+    if not resultset_response:
+        continue
+    resultset = resultset_response['results'][0]
+    print resultset['push_timestamp'], resultset['revision']
+    resultsets.append(resultset)
 
 # Sort by push timestamp since a nightly could be triggered on an older revision later (e.g. if the newer revision is busted).
 sorted_resultsets = sorted(resultsets, key=lambda resultset: resultset['push_timestamp'])
@@ -37,7 +40,7 @@ for i, resultset in enumerate(sorted_resultsets):
     oldRev = sorted_resultsets[i - 1]['revision']
     newRev = resultset['revision']
     print i, resultset['push_timestamp'], resultset['revision']
-    print "https://screenshots.mattn.ca/compare/?oldProject=%s&oldRev=%s&newProject=%s&newRev=%s" % (project, sorted_resultsets[i - 1]['revision'], project, resultset['revision'])
+    print "https://screenshots.mattn.ca/compare/?oldProject=%s&oldRev=%s&newProject=%s&newRev=%s" % (project, oldRev, project, newRev)
     compare_screenshots.cli(args=[os.path.join(archive, project, oldRev),
                                   os.path.join(archive, project, newRev),
                                   "--output",
