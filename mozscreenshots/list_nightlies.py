6141152a590889a6168692188ff06ee7be56e6b8
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2017-02-25 01:59:35 -0500

    nightlies: Exclude known inconsistencies

--- mozscreenshots/list_nightlies.py
+++ mozscreenshots/list_nightlies.py
@@ -3,9 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import datetime
+import json
 import os
+import re
 import sys
+import time
 from collections import namedtuple
+from pytz import timezone
 
 import compare_screenshots
 from compare_screenshots import ComparisonResult, comparisonResultNames
@@ -14,69 +18,118 @@ from fetch_screenshots import nightly_revs_for_date, resultset_response_for_push
 archive = os.getcwd()
 compare_url_format = "https://screenshots.mattn.ca/compare/?oldProject=%s&oldRev=%s&newProject=%s&newRev=%s"
 project = "mozilla-central"
-base = datetime.date.today()
+base = datetime.date.today() # datetime.date(2017, 1, 19)
 numdays = 4
+timezone = timezone('US/Pacific')
+
 resultsets = []
 nightly_revs = set()
 
 
-def email_results(project, oldRev, newRev, comparison):
+def email_results(project, oldResultset, newResultset, comparison, known_inconsistencies):
+    import bleach
     import email
-    import json
     import re
     import smtplib
 
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
 
+    oldRev = oldResultset['revision']
+    newRev = newResultset['revision']
+
     # Used to only send an email with at least one difference.
     difference_found = False
-    body = "Screenshot changes between {} revisions {} and {}\n".format(project,
-                                                                        oldRev[:12],
-                                                                        newRev[:12])
-    body += compare_url_format % (project, oldRev, project, newRev) + "\n\n"
+
+    body =  "Project:       {}\n".format(project)
+    body += "Base revision: {} ({})\n".format(oldRev, datetime.datetime.fromtimestamp(oldResultset['push_timestamp'], timezone))
+    body += "New revision:  {} ({})\n".format(newRev, datetime.datetime.fromtimestamp(newResultset['push_timestamp'], timezone))
+    body += "Details:       "
+    body += compare_url_format % (project, oldRev, project, newRev) + "\n"
+    body += "Pushlog:       "
+    body += "https://hg.mozilla.org/{}/pushloghtml?fromchange={}&tochange={}\n\n".format(project, oldRev, newRev)
 
     for dir, results in comparison.iteritems():
+        platform = os.path.basename(dir)
         differences = ""
 
         nonsimilar = dict((key.replace(".png", ""), value) for key, value in results.iteritems()
                           if value["result"] != ComparisonResult.SIMILAR)
-        imagenamecolumnwidth = reduce(lambda x, y: max(x, len(y)), nonsimilar.keys(), 0)
 
+        rows = []
         for imagename in sorted(nonsimilar.keys()):
             result = nonsimilar[imagename]
             label = comparisonResultNames[result["result"]]
 
             if result["result"] == ComparisonResult.DIFFERENT:
-                differences += "{} {} ({})\n".format(imagename.ljust(imagenamecolumnwidth),
-                                                     label,
-                                                     result["difference"])
-            else:
-                differences += "{} {}\n".format(imagename.ljust(imagenamecolumnwidth), label)
+                if matches_inconsistency(known_inconsistencies, platform, imagename, result):
+#                    print "ignoring", imagename, "on", platform, "with", result['difference']
+                    continue
 
-        if differences:
+                rows.append((imagename, label, result["difference"]))
+            else:
+                rows.append((imagename, label))
+
+        if len(rows) > 0:
             difference_found = True
         else:
             continue
-        body += "== " + os.path.basename(dir) + " ==\n"
-        body += differences
+
+        imagenamecolumnwidth = max(30, reduce(lambda x, y: max(x, len(y[0])), rows, 0))
+        body += "== " + platform + " ==\n"
+        for row in rows:
+            body += "{}  {}".format(row[0].ljust(imagenamecolumnwidth), row[1])
+            if (len(row) == 3):
+                body += " ({})".format(row[2])
+            body +="\n"
         body += "\n\n"
 
     if not difference_found:
+        print "\nNo differences found\n\n"
         return
 
+    print "====\n"
+    print body
+
     msg = MIMEMultipart('alternative')
+    #msg['Date'] = email.utils.formatdate(float(newResultset['push_timestamp']))
     msg['Message-ID'] = re.sub("@[^>]+>", "@screenshots.mattn.ca>", email.utils.make_msgid())
     msg['Subject'] = 'Nightly Screenshot Changes: {} to {}'.format(oldRev[:12], newRev[:12])
     msg['From'] = 'Nightly Screenshot Changes <nightly-changes@screenshots.mattn.ca>'
     msg['To'] = 'dev-ui-alerts@lists.mozilla.org'
 
     msg.attach(MIMEText(body, 'plain'))
-    msg.attach(MIMEText('<pre>' + body + '</pre>', 'html'))
+    msg.attach(MIMEText('<pre>\n' + bleach.linkify(body) + '</pre>', 'html'))
 
     s = smtplib.SMTP('localhost', None, 'screenshots.mattn.ca')
     s.sendmail(msg['From'], [msg['To']], msg.as_string())
     s.quit()
+    # Delay before the next email so they get received in the correct order
+    time.sleep(60)
+
+def matches_inconsistency(inconsistencies, platform, basename, result):
+    for known in inconsistencies:
+        if not known['platformRegex'].search(platform):
+            continue
+
+        if not known['pixelRegex'].search(str(result['difference'])):
+            continue
+
+        if not any(pattern.search(basename) for pattern in known['nameRegexes']):
+            continue
+        return True
+
+    return False
+
+# Load the known inconsistencies file
+known_inconsistencies_path = os.path.join(os.path.dirname(__file__), "..", "web", "known_inconsistencies.json")
+with open(known_inconsistencies_path, 'r') as ki_file:
+    known_inconsistencies = json.load(ki_file)
+    print "Known inconsistencies:", known_inconsistencies
+    for known in known_inconsistencies:
+        known['platformRegex'] = re.compile(known['platformRegex'])
+        known['pixelRegex'] = re.compile(known['pixelRegex'])
+        known['nameRegexes'] = map(lambda pattern: re.compile(pattern), known['nameRegexes'])
 
 for offset in range(0, numdays):
     d = base - datetime.timedelta(days=offset)
@@ -91,6 +144,9 @@ for rev in nightly_revs:
         continue
     resultset = resultset_response['results'][0]
     print resultset['push_timestamp'], resultset['revision']
+    if len(os.listdir(os.path.join(archive, project, resultset['revision']))) == 0:
+        print 'No subdirs for', resultset['revision']
+        continue
     resultsets.append(resultset)
 
 # Sort by push timestamp since a nightly could be triggered on an older revision
@@ -101,7 +157,8 @@ print
 for i, resultset in enumerate(sorted_resultsets):
     if i == 0:
         continue
-    oldRev = sorted_resultsets[i - 1]['revision']
+    oldResultset = sorted_resultsets[i - 1]
+    oldRev = oldResultset['revision']
     newRev = resultset['revision']
     print i, resultset['push_timestamp'], resultset['revision']
     print compare_url_format % (project, oldRev, project, newRev)
@@ -114,10 +171,12 @@ for i, resultset in enumerate(sorted_resultsets):
             print('Error creating directory: %s' % outdir)
             sys.exit(1)
 
-    CompareDirOptions = namedtuple('CompareDirOptions', 'dppx overwrite')
-    options = CompareDirOptions(dppx=1.0, overwrite=False)
+    CompareDirOptions = namedtuple('CompareDirOptions', 'dppx overwrite include_completed')
+    options = CompareDirOptions(dppx=1.0, overwrite=False, include_completed=True)
 
     comparison = compare_screenshots.compare_dirs(os.path.join(archive, project, oldRev),
                                                   os.path.join(archive, project, newRev),
                                                   outdir, options)
-    email_results(project, oldRev, newRev, comparison)
+    lastRev = newRev
+
+    email_results(project, oldResultset, resultset, comparison, known_inconsistencies)
